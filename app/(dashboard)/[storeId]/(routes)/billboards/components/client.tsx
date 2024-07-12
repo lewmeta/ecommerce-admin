137e372269821ff159f53f85a4ba797723744379
thegypsyboyo <lewisonyango9@gmail.com>
2024-07-12 21:06:26 +0300

    we have created an component to display the api endpoints and modified middlware to work only for GET public api routes

--- app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -1,13 +1,14 @@
 "use client"
 
 import { Plus } from "lucide-react"
+import { useParams, useRouter } from "next/navigation"
 
 import { Button } from "@/components/ui/button"
 import { Heading } from "@/components/ui/heading"
-import { useParams, useRouter } from "next/navigation"
 import { BillboardColumns, columns } from "./columns"
 import { Separator } from "@/components/ui/separator"
 import { DataTable } from "@/components/ui/data-table"
+import { ApiList } from "@/components/ui/api-list"
 
 interface BillboardClientProps {
     data: BillboardColumns[];
@@ -34,6 +35,14 @@ export const BillboardClient: React.FC<BillboardClientProps> = ({
             </div>
             <Separator />
             <DataTable columns={columns} data={data} searchKey="label" />
+            <Heading
+                title="API"
+                description="API calls from the billboard"
+            />
+            <ApiList
+                entityIdName="billboardId"
+                entityName="billboards"
+            />
         </>
     )
 }
